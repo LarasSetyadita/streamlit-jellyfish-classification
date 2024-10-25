995edcb703ff9b6e0f7dcb6f69fd13683059fae0
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-10-25 20:56:46 +0700

    add pages file

---
+++ dashboard.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+#########################
+# pengaturan tampilan #
+#########################
+st.set_page_config(layout='wide')
+
+###############
+# main method #
+###############
+def main():
+    st.title('🌊 Jellyfish Classification Dashboard 🪼')
+    st.write('hello world')
+
+#########################
+# menjalankan kode main #
+#########################
+if __name__ == '__main__':
+    main()
